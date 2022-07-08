57d4c1b83504ac780645161db18e061e9d4f00b9
Ilia Vorontcov <joker.pnz@gmail.com>
2022-07-07 20:31:53 -0400

    Update readme, write some tests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-hr
 Test assignment app
+
+## Instructions
+Run `npm install` and then `npm run start` to run the application.
+
+To run tests use `npm run test` command.
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders landing page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByRole("button", { name: "GO!" });
+  expect(button).toBeInTheDocument();
+});
+
+test("displays profile photo fetched from the server", async () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("GO!"));
+
+  // check if avatar displayed
+  const avatar = await screen.findByRole("img", { class: "card-img-top" });
+  expect(avatar).toBeInTheDocument();
+
+  // check if ACCEPT button exists
+  const acceptBtn = screen.getByRole("button", {
+    name: "ACCEPT"
+  });
+  expect(acceptBtn).toBeInTheDocument();
+
+  // check if REJECT button exists
+  const rejectBtn = screen.getByRole("button", {
+    name: "REJECT"
+  });
+  expect(rejectBtn).toBeInTheDocument();
+
+  // check if counter increased after ACCEPT button click
+  fireEvent.click(acceptBtn);
+  fireEvent.click(acceptBtn);
+  fireEvent.click(acceptBtn);
+  const counter = screen.getByText("3");
+  expect(counter).toBeInTheDocument();
 });
